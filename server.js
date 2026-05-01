9b5533ea6f6a4c2f4f45cb68024dcb8936429d4b
Raghavan <yourgithubemail@gmail.com>
2026-05-02 01:02:41 +0530

    changes completed for render

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv');
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const chatRoutes = require('./routes/chatRoutes');
 const authRoutes = require('./routes/authRoutes');
-
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
